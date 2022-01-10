fc9088ec0edf889881d3fe2634be3b5066cc6b91
Igor Kagan <igor.b.kagan@gmail.com>
2022-01-10 15:09:11 +0100

    temp

--- MP_inputdlg.m
+++
@@ -1,7 +0,0 @@
-function answer = MP_inputdlg(matlabversion,varargin)
-if matlabversion(1)>=2014
-answer = dag_inputdlg(varargin{:});    
-else
-answer = inputdlg(varargin{:});
-end
-end
--- ig_add_multiple_vertical_lines.m
+++
@@ -1,6 +0,0 @@
-function ig_add_multiple_vertical_lines(vertical_lines_x,varargin)
-
-ylim = get(gca,'Ylim');
-
-
-line(kron(vertical_lines_x,[1 1 1]),repmat([ylim NaN],1,length(vertical_lines_x)),varargin{:});
